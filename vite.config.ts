5d2194d1500a022d5dc946cafd59ce2e40b96524
miaokl <2972799448@qq.com>
2025-08-04 16:51:01 +0800

    🐛 fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -12,20 +12,7 @@ export default defineConfig({
   },
   build: {
     outDir: path.resolve(__dirname, "dist/renderer"),
-    emptyOutDir: true,
-    lib: {
-      entry: path.resolve(__dirname, "src/main.ts"),
-      formats: ["cjs"],
-      fileName: () => "main.cjs",
-    },
-    rollupOptions: {
-      // 不把 electron 或 Node 内置模块一起打包
-      external: ["electron"],
-      output: {
-        // CommonJS 专用导出方式
-        exports: "auto",
-      },
-    },
+    emptyOutDir: true
   },
   server: { port: 3001 },
 });
